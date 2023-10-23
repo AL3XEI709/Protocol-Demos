1504aeed0a96ea8cf22dae4e33189a438ef9bf3c
alekseypeng <2084308740@qq.com>
2023-10-23 13:44:22 +0800

    newest

--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
-echo 打开clash操作
+echo open in clash
 git config --global http.proxy socks5 127.0.0.1:7890
 git config --global https.proxy socks5 127.0.0.1:7890
 git config --global http.proxy 127.0.0.1:7890
